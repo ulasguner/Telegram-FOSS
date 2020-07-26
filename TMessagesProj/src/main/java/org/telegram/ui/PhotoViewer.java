6ba63db9ff30855764b0a955c5a641268a02d3df
thermatk <thermatk@thermatk.com>
2020-07-26 18:16:32 +0200

    [TF][KILL] Google Vision

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -125,9 +125,6 @@ import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.analytics.AnalyticsListener;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.Bitmaps;
@@ -8180,6 +8177,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
             return;
         }
         Utilities.globalQueue.postRunnable(() -> {
+        	/*
             FaceDetector faceDetector = null;
             try {
                 faceDetector = new FaceDetector.Builder(ApplicationLoader.applicationContext)
@@ -8200,7 +8198,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 } else {
                     if (BuildVars.LOGS_ENABLED) {
                         FileLog.e("face detection is not operational");
-                    }
+                    }*/
                     AndroidUtilities.runOnUIThread(() -> {
                         bitmap.release();
                         String imageKey = centerImage.getImageKey();
@@ -8208,7 +8206,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                             currentImageHasFace = 2;
                             currentImageFaceKey = key;
                         }
-                    });
+                    });/*
                 }
             } catch (Exception e) {
                 FileLog.e(e);
@@ -8216,7 +8214,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 if (faceDetector != null) {
                     faceDetector.release();
                 }
-            }
+            }*/
         });
     }
 
